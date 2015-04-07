7d795fa2819e079a483e1f2626983fb2f449e97d
Srinivas Garlapati <srinivasa.b.garlapati@gmail.com>
2015-04-07 09:39:14 +0530

    Connection restricted to Mysql at this time.

--- integrator/integrator/platform/db_engine/connections.py
+++ integrator/integrator/platform/db_engine/connections.py
@@ -1,8 +1,6 @@
 from sqlalchemy import create_engine
 
-# Sqlite engine
-engine = create_engine('sqlite:///:memory:', echo=True)
 
 # Mysql engine
-mysql = create_engine("mysql://root:pwd@localhost/test",
+MYSQL = create_engine("mysql://root:pwd@localhost/test",
                       encoding='latin1', echo=True)
